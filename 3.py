f5614b8ad14e4b74c1f31e34a0e0bcdff11141d1
dekkona <101133175+dekkona@users.noreply.github.com>
2022-11-24 16:51:05 -0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,31 +1,47 @@
-import pymongo
 
-client = pymongo.MongoClient('mongodb://localhost:27017')
-db = client['291db']
-collection = db['dblp']
+def list_venues(db):
+    collec = db['dblp'] 
 
-n = int(input("Enter the number of venues to list: "))
+    # get distinct venues for venue_count
+    venues = collec.distinct("venue")
+    venue_count = len(venues)
+    # get user input for top n venues
+    valid_n = False
+    while not valid_n:
+        inp_n = input("Enter a number n and see a listing of top n venues: ")
+        try:
+            inp_n = int(inp_n)
+            if inp_n > venue_count:
+                print("n exceeds number of venues. Please enter a smaller number.")
+            elif inp_n <= 0:
+                print("n must be greater than zero.")
+            else:
+                valid_n = True
+        except ValueError:
+            print("n must be an integer.")    
 
-pipeline = [
-    {
-        "$match": {"venue": "international conference on human-computer interaction"}
-    },
+    # for each distinct venue get count
+    top_venues = list(collec.aggregate([{"$group": {"_id": "$venue", "count": {"$sum": 1}}}, {"$sort": {"count": -1}}, {"$limit": inp_n}]))
+    print(top_venues)
+    top_refs = {}
+    top_count = {}
+    # get articles that fall under top n venues
+    for venue in top_venues:
+        articles = list(collec.aggregate([{'$match': {'venue': {'$eq': venue['_id']}}}, {'$project': {"id": '$id', '_id': 0}}]))
+        ids = []
+        for article in articles:
+            ids.append(article["id"])
+        
+        article_refs = list(collec.aggregate([{'$match': {'references': {'$elemMatch': {'$in': ids}}}}]))
+        top_count.update({venue['_id']: venue['count']})
+        top_refs.update({venue['_id']: len(article_refs)})
     
-    {
-        "$limit": n
-    }
-]
+    #top_refs = {k: v for k, v in sorted(top_refs.items(), key=lambda item: item[1][1], reverse = True)}
+    print(top_count)    
+    print(top_refs)
 
-results = collection.aggregate(pipeline)
-
-print("Result Number | Venue | Number of Articles | Number of References")
-print("-----------------------------------------------------------------")
-
-i = 0
-for result in results:
-    num_articles = collection.aggregate({"$count": {}})
-   
-    string = "{:<13} | {:<25} | {:<13} | {:<13}".format(i, result['venue'], )
-    print(result['title'])
-
-    i += 1
+    i=1
+    print('\nTop Venues')
+    for k, v in top_count.items():
+        print(('-'*50)+'\n#'+str(i)+ '\nVenue: ' + k + '\nArticles in Venue: ' + str(top_count[k]) + '\nReferences: ' + str(top_refs[k]))
+        i += 1
