9469ff25a5f2f0c6532eb35fd8c4974e34d372be
VinKurup <89051502+VinKurup@users.noreply.github.com>
2022-11-24 15:50:36 -0700

    Create list_venue.py
    lists top n venues

---
+++ list_venue.py
@@ -0,0 +1,29 @@
+def list_venue(db):
+    ven = db.dblp.aggregate([{"$group": {"_id": "$venue", "count": {"$sum": 1}}},
+                             {"$project": {"venue": "$_id", "count": 1, "_id": 0}},
+                             {"$sort": {"venue": -1}}])
+    val = False
+    n_venue = list(ven)
+    try:
+        while not val:
+            n = int(input("Enter the number n of top venues to see list:"))
+            if n >= len(n_venue):
+                print("n exceeds venue count. Please enter smaller number.")
+            elif n <= 0:
+                print("n must be greater than 0")
+            else:
+                val = True
+    except(Exception):
+        print("Invalid entry")
+        return
+    venue_refs = db.vendb.aggregate([{"$lookup":
+                             {"from": "dblp",
+                              "localField": "id",
+                              "foreignField": "references",
+                              "as": "refs"}},
+                        {"$group":
+                             {"_id": "$venue"}, "references": {"$sum": {"$size": "$refs"}}},
+                        {"$project":{"venue": "$_id", "references": 1, "_id": 0}},
+                                     {"$sort": {"venue": -1}}, {"$limit": n}])
+    for i, j in venue_refs, n_venue:
+        print({"venue: " + i["venue"] + " articles: " + j["count"] + " referenced by: " + i["references"]})
