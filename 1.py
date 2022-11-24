052f20f731ddd1b501d901c9a8f5fd63e5b4c326
VinKurup <89051502+VinKurup@users.noreply.github.com>
2022-11-23 17:52:25 -0700

    1.py
    updated references finder

--- 1.py
+++ 1.py
@@ -29,4 +29,37 @@ def searchArticles(db):
         # return to main menu here
     else:
         pass
+        # return to main menu heredef searchArticles(db):
+    word = input("Search for an article: ")
+    try:
+        word = int(word)
+        results = db.dblp.find({"year": word})
+    except:
+        query = {"$search": "(\"{}\"".format(word)}
+        results = db.dblp.find({"$text": query},
+                                {"_id": 0, "id": 1, "title": 1, "year": 1, "venue": 1})
+    count = 1
+    resArr = list(results)
+    for i in resArr:
+        print(f"{count}. {i}")
+        count += 1
+    opt = int(input("Select an article to view more. 0 to return to main menu: "))
+    if opt in range(1, len(resArr) + 1):
+        get_one = db.dblp.find_one({"id": resArr[opt - 1].get('id')},
+                               {"_id": 0, "id": 1, "title": 1, "abstract": 1, "venue": 1, "authors": 1,
+                                "year": 1})
+        print(get_one)
+        # refs = db.dblp.aggregate([{"$unwind": "$references"},
+        #                           {"$match": {"references": resArr[opt - 1].get('id')}},
+        #                           {"$group": {"_id": "$references"}},
+        #                           {"$project": {"_id": 0, "id": 1, "title": 1, "year": 1}}
+        # ])
+        refs = db.dblp.find({"references": resArr[opt - 1].get('id')},
+                            {"_id": 0, "title": 1, "year": 1, "id": 1})
+        print("Article referenced by:")
+        for i in refs:
+            print(i)
+        # return to main menu here
+    else:
+        pass
         # return to main menu here
